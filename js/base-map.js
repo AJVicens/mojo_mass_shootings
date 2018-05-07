898d7944a9996591f8c5ed378f57ba000cb46b7a
AJ Vicens <avicens@FNP00454.local>
2018-05-07 15:48:27 -0400

    tweaked default zoom for mobile

--- js/base-map.js
+++ js/base-map.js
@@ -1,11 +1,11 @@
 // Set view of Leaflet map based on screen size
 var layer = new L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
     attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    minZoom: 4,
+    minZoom: 3,
     maxZoom: 9
 });
 if ($(window).width() < 626) {
-    var map = new L.Map('map').setView([42,-93],6);
+    var map = new L.Map('map').setView([42,-96],3);
 } else {
     var map = new L.Map('map').setView([42,-96.5],5);
 }
